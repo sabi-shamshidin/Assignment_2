aea2cf11f4a9445c52d7a0dbc97bea2c6b92cc0d
sabi-shamshidin <82763714+sabi-shamshidin@users.noreply.github.com>
2021-10-11 01:12:19 +0600

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -3,26 +3,10 @@ import requests
 from fake_useragent import UserAgent
 
 
-class WebScrapper:
-    def get_currencies(self):
-        response = requests.get("https://coinmarketcap.com/all/views/all/")
-        html_soup = BeautifulSoup(response.text, 'html.parser')
-
-        tr_elements = html_soup.find_all('table')[2].find_all('tr')
-        data = []
-
-        for row in tr_elements:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            data.append([ele for ele in cols if ele])
-
-        data.pop(0)
-        del data[20:]
-        return data
+class WebParser:
 
     def get_currency(self, symbol):
         data = self.get_currencies()
-
         for i in range(20):
             if data[i][2] == symbol:
                 return data[i]
@@ -30,18 +14,15 @@ class WebScrapper:
     def search_for_google(self, query, number_result):
         query = query.replace(' ', '+')
         ua = UserAgent()
-
         google_url = "https://www.google.com/search?q=" + query + "&num=" + str(number_result)
         response = requests.get(google_url, {"User-Agent": ua.random})
         soup = BeautifulSoup(response.text, "html.parser")
 
         result_div = soup.find_all('div', attrs={'class': 'ZINbbc'})
-
         links = []
         titles = []
         descriptions = []
         for r in result_div:
-
             try:
                 link = r.find('a', href=True)
                 title = r.find('div', attrs={'class': 'vvjwJb'}).get_text()
@@ -51,7 +32,6 @@ class WebScrapper:
                     links.append(link['href'])
                     titles.append(title)
                     descriptions.append(description)
-
             except:
                 continue
 
