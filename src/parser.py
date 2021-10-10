3ddbd71f9bebe1f6bc2442657e09f39ff19746c5
sabi-shamshidin <82763714+sabi-shamshidin@users.noreply.github.com>
2021-10-11 01:17:24 +0600

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -19,24 +19,20 @@ class WebParser:
         soup = BeautifulSoup(response.text, "html.parser")
 
         result_div = soup.find_all('div', attrs={'class': 'ZINbbc'})
-        links = []
         titles = []
-        descriptions = []
+        links = []
         for r in result_div:
             try:
                 link = r.find('a', href=True)
                 title = r.find('div', attrs={'class': 'vvjwJb'}).get_text()
-                description = r.find('div', attrs={'class': 's3v9rd'}).get_text()
 
                 if link != '' and title != '' and description != '':
-                    links.append(link['href'])
                     titles.append(title)
-                    descriptions.append(description)
+                    links.append(link['href'])
             except:
                 continue
 
         for i in range(len(descriptions)):
             print("Title: ", titles[i])
-            print("Description: ", descriptions[i])
             print("Link: ", links[i])
-            print("--------------------------------------------------------------------------------------------------")
+            print("_________________________________________________________________________________________")
